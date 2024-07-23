f61e2cc6920ce8b4fa26b0d574c765b3c45dec85
Kashif <ahmed.kashif1001@gmail.com>
2024-07-24 00:58:00 +0500

    screen route params

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
-    <View style={{marginTop:40}}>
-      <Text>HomeScreen</Text>
+    <View style={styles.viewStyle}>
+    <Text>HomeScreen</Text>
+    <Button title='Profile' onPress={()=>navigation.navigate('Profile')}/>
+      <View style={{margin:10}}>
+    <Button title='User' onPress={()=>navigation.navigate('User')}/>
     </View>
+  </View>
   )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    display:'flex',
+    marginTop:40,
+    justifyContent:'center',
+    alignItems:'center',
+    flex:1
+  }
+})
